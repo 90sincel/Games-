51ec8fab73c7ff5365a560c2e071c0801fe437da
Trevor Hegarty <tomheg97@protonmail.com>
2020-01-14 15:09:29 -0500

    Boredless first commit

---
+++ code.prog/Python/boredless.py
@@ -0,0 +1,6 @@
+Python 3.7.6 (default, Dec 30 2019, 19:38:28) 
+[Clang 11.0.0 (clang-1100.0.33.16)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable. Visit
+http://www.python.org/download/mac/tcltk/ for current information.
+>>> 
