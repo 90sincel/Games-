7f7cb566f76d939bb2ecb6076db097dfd018bf28
Trevor Hegarty <tomheg97@protonmail.com>
2020-01-14 15:31:25 -0500

    Get_destination commit

--- code.prog/Python/boredless.py
+++ code.prog/Python/boredless.py
@@ -3,4 +3,10 @@ Python 3.7.6 (default, Dec 30 2019, 19:38:28)
 Type "help", "copyright", "credits" or "license()" for more information.
 WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable. Visit
 http://www.python.org/download/mac/tcltk/ for current information.
->>> 
+>>> destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paolo, Brazil', 'Cairo, Egypt']
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
